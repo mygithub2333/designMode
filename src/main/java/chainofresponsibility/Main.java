a3282dbb9a74fa55aa1c1d6c698bd287b3569195
fuwenxin <1281370249@qq.com>
2022-02-24 17:20:07 +0800

    feat:新增责任链模式

---
+++ src/main/java/chainofresponsibility/Main.java
@@ -0,0 +1,115 @@
+package chainofresponsibility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author fwx
+ * @date 2022/2/24
+ */
+public class Main {
+    public static void main(String[] args) {
+        Msg msg = new Msg();
+        msg.setMsg("大家好:),<script>,欢迎访问 mashibing.com，大家都是996");
+        FilterChain fc = new FilterChain();
+        fc.add(new HTMLFilter()).add(new SensitiveFilter());
+        FilterChain fc1 = new FilterChain();
+        fc1.add(new FaceFilter()).add(new URLFilter());
+        fc.add(fc1);
+        boolean b = fc.doFilter(msg);
+        System.out.println(b);
+        System.out.println(msg);
+
+    }
+}
+class Msg{
+    String msg;
+    String name;
+
+    @Override
+    public String toString() {
+        return "Msg{" +
+                "msg='" + msg + '\'' +
+                '}';
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+interface Filter{
+    boolean doFilter(Msg msg);
+}
+
+class HTMLFilter implements Filter{
+    @Override
+    public boolean doFilter(Msg msg) {
+        String r = msg.getMsg();
+        r = r.replace('<','[');
+        r = r.replace('>',']');
+        msg.setMsg(r);
+        return true;
+    }
+}
+
+class SensitiveFilter implements Filter{
+    @Override
+    public boolean doFilter(Msg msg) {
+        String r = msg.getMsg();
+        r = r.replaceAll("996","955");
+        msg.setMsg(r);
+        return true;
+    }
+}
+
+class FaceFilter implements Filter{
+    @Override
+    public boolean doFilter(Msg msg) {
+        String r = msg.getMsg();
+        r = r.replace(":)","^v^");
+        msg.setMsg(r);
+        return true;
+    }
+}
+
+class URLFilter implements Filter{
+    @Override
+    public boolean doFilter(Msg msg) {
+        String r = msg.getMsg();
+        r = r.replace("mashibing.com","http://www.mashibing.com");
+        msg.setMsg(r);
+        return true;
+    }
+}
+
+class FilterChain implements Filter{
+    List<Filter> filters = new ArrayList<>();
+
+    public FilterChain add(Filter f){
+        filters.add(f);
+        return this;
+    }
+
+    @Override
+    public boolean doFilter(Msg msg) {
+        for (Filter filter : filters) {
+            if (!filter.doFilter(msg)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
